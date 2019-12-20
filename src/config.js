904314c0e5ca72ec5df820ae4fe2c1330f47448f
unknown <gurbuz15@itu.edu.tr>
2019-12-20 17:17:53 +0300

    sahdk

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const production = {
     LOGIN_URL: 'https://qrpilot-back.appspot.com/login/',
     SIGNUP_URL : 'https://qrpilot-back.appspot.com/signUp',
     GAME_URL: 'https://qrpilot-back.appspot.com/game',
-    GET_LOCATION_URL: "https://qrpilot-back.appspot.com/get-location/"
+    UPDATE_LOCATION_URL: "https://qrpilot-back.appspot.com/update-location/"
 };
 
 const config = {
